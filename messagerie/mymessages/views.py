e3c547411f755bea661526e00398b75fd80b0d38
alphamanjaka <alphamanjaka@gmail.com>
2025-12-22 12:17:51 +0300

    feat: Enhance logging in home view for message handling and retrieval

--- messagerie/mymessages/views.py
+++ messagerie/mymessages/views.py
@@ -4,9 +4,11 @@ from .models import Message
 
 def home(request):
     if request.method == "POST":
+        print("Contenu du message reçu:", request.POST.get("contenu"))
         contenu = request.POST.get("contenu")
         Message.objects.create(contenu=contenu)
         return redirect('home')
-
+    # On récupère tous les messages
+    print("Récupération des messages depuis la base de données")
     messages_liste = Message.objects.all().order_by('-date_envoi')
     return render(request, 'index.html', {'messages_liste': messages_liste})
