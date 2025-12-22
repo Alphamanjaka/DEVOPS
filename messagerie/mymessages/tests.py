82c4d5a565a7fb94785ca995a1a7d7f40483e6fd
alphamanjaka <alphamanjaka@gmail.com>
2025-12-22 10:20:02 +0300

    feat: Implement messaging functionality with views, templates, and tests

--- messagerie/mymessages/tests.py
+++ messagerie/mymessages/tests.py
@@ -8,3 +8,17 @@ class MessageTest(TestCase):
         msg = Message.objects.create(contenu="Hello DevOps")
         # On vérifie que le message est bien enregistré
         self.assertEqual(msg.contenu, "Hello DevOps")
+
+    def test_affichage_messages(self):
+        # On crée plusieurs messages
+        Message.objects.create(contenu="Message 1")
+        Message.objects.create(contenu="Message 2")
+        # On récupère tous les messages
+        messages = Message.objects.all()
+        # On vérifie qu'il y a bien 2 messages
+        self.assertEqual(messages.count(), 2)
+
+    def test_page_accueil_charge(self):
+        response = self.client.get('/')
+        # Vérifie que la page s'affiche sans erreur
+        self.assertEqual(response.status_code, 200)
--- messagerie/mymessages/views.py
+++ messagerie/mymessages/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Message
 
-# Create your views here.
+
+def home(request):
+    if request.method == "POST":
+        contenu = request.POST.get("contenu")
+        Message.objects.create(contenu=contenu)
+        return redirect('home')
+
+    messages_liste = Message.objects.all().order_by('-date_envoi')
+    return render(request, 'index.html', {'messages_liste': messages_liste})
