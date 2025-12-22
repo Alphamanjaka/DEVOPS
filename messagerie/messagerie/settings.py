160a909ee8fd61977ab1465d26deaf5d9557ac80
alphamanjaka <alphamanjaka@gmail.com>
2025-12-22 12:29:32 +0300

    feat: Set DEBUG to True for development environment in settings.py

--- messagerie/messagerie/settings.py
+++ messagerie/messagerie/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1=aoe*64osbslt8)xc(rf-o7&922vh6=fn9u@xzx0czpndtdg1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
