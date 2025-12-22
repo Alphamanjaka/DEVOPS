b76ea96912fe4f410315893ab89ceaf2fc400801
alphamanjaka <alphamanjaka@gmail.com>
2025-12-22 16:07:40 +0300

    update setting database fixing

--- messagerie/messagerie/settings.py
+++ messagerie/messagerie/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -86,6 +87,9 @@ DATABASES = {
     }
 }
 
+# Si la variable d'environnement DATABASE_URL existe (comme sur Render), elle écrase la config ci-dessus
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
