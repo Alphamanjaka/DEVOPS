f8159b609584deb37c1d121dc9d6a257336c2152
alphamanjaka <alphamanjaka@gmail.com>
2025-12-25 20:30:46 +0300

    Add superuser creation step in build script

--- build.sh
+++ build.sh
@@ -11,6 +11,10 @@ python manage.py migrate --noinput
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
+# Créer un superutilisateur par défaut si nécessaire
+echo "Creating default superuser..."
+python manage.py createsuperuser --no-input || true
+
 # Lancer Gunicorn pour servir l'application
 echo "Starting Gunicorn..."
 gunicorn --bind 0.0.0.0:8000 --reload messagerie.wsgi:application
